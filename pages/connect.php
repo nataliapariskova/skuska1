b7e40a887830e225a2970ad201bdfcb313e46b04
natalia.pariskova@gmail.com <natalia.pariskova@gmail.com>
2019-03-25 15:13:35 +0100

    prepojenie s azure

--- pages/connect.php
+++ pages/connect.php
@@ -5,6 +5,20 @@ $username = "root";
 $password = "";
 $dbname = "auta";
 
+//vložené z php my admin, azure aby sme sa napojili na databázu v azure
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
